8f7db0e9151a1e57f2a163e28c420d38e0fc63c2
HydE <chucha1204@gmail.com>
2026-02-23 21:01:17 +0100

    setup

---
+++ index.js
@@ -0,0 +1,14 @@
+// require("dotenv").config();
+// const http = require("http");
+
+// const port = process.env.PORT || 3333;
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader("Content-Type", "text/plain");
+//   res.end("Server started...");
+// });
+
+// server.listen(port, () => {
+//   console.log(`Server started at http://localhost:${port}`);
+// });
